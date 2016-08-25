8825704e51b8fc90e5da447bc1f255014cac0b67
manuelabuyan <manuel.abuyan@gmail.com>
2016-08-25 18:09:07 +1200

    Completed code

--- lift_controller.c
+++ lift_controller.c
@@ -21,6 +21,7 @@ Direction requestedDirection = UP;
 int elevatorButtons[] = {0,0,0,0};
 int callButtons[] = {0,0,0,0,0,0,0,0};
 int callButtonPriority = 1;
+int doorOpen;
 FILE* fp;
 alt_alarm timer; // timer
 
@@ -43,6 +44,12 @@ alt_u32 timeout_function(void* context)
 	// Write your own code here to :
 	// 1. Implement functionality to close the door when timeout  happens
 	// 2. Return an appropriate value for this function
+	 char* formattedString = "Closing Door\n";
+	 fprintf(fp,"%s",formattedString);
+
+	 doorOpen = 0;
+
+	 return 0;
 }
 
 
@@ -57,6 +64,20 @@ alt_u32 timeout_function(void* context)
 void handle_button_interrupts(void* context, alt_u32 id){
   //Reset the edge capture register on button PIO 1
   IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE, 0);
+
+  //TEST HERE
+  /*
+  char* formattedString = "Motor is working:\n\n";
+
+
+     fp = fopen("/dev/uart", "r+");
+
+ 	if (fp != NULL) {
+ 		fprintf(fp,"%s",formattedString );
+ 	}
+
+
+ //TEST HERE*/
 
   switch (motorDirection){
   case UP:
@@ -88,6 +109,14 @@ void init_button_interrupts(){
    * handle_button_interrupts(void* context, alt_u32 id) is already provided
    * to handle this interrupt.
    */
+
+  //Clearing the edge capture register
+  IOWR_ALTERA_AVALON_PIO_EDGE_CAP(BUTTONS_BASE, 0);
+  //Enable interrups for button one
+  IOWR_ALTERA_AVALON_PIO_IRQ_MASK(BUTTONS_BASE, 0x2);
+  //Register the ISR
+  alt_irq_register(BUTTONS_IRQ, NULL, handle_button_interrupts);
+
 }
 
 
@@ -122,11 +151,9 @@ void callButtons_add(int floor, int down){
  * Outputs:     The priority of the call button request
  ************************************************************************/
 int callButtons_priority(int floor, int down){
-  /* write your own code here that returns the priority of the specified
-   * call button.
-   */
-   index = 2 * (floor - 1) + down;
-   return callButtons[index];
+	int index = 2 * (floor - 1) + down;
+	return callButtons[index];
+
 }
 
 
@@ -145,21 +172,24 @@ void callButtons_remove(int floor, int down){
    * call button once it is serviced and adjusts the priority of all
    * other waiting requests.
    */
-   
-    //Sets specified cell to 0
-	index = 2 * (floor - 1) + down;
-	callButtons[index] = 0;
-	
+
+    //gets index of floor in callButtons
+	int index = 2 * (floor - 1) + down;
+
+
 	//Decrement callButtonPriority
 	callButtonPriority--;
-	
+
 	//Loops through the callButtons array and decrements all values by 1
+	int i;
 	for (i = 0; i < 8; i++) {
-		if (callButtons[i] == 0) {
-			continue;
+		if ((callButtons[i] != 0) && (callButtons[i] > callButtons[index]) && (callButtons[i] != callButtons[index])) {
+			callButtons[i] = callButtons[i] - 1;
 		}
-		callButtons[i] == callButtons[i] - 1;
 	}
+
+	//set specific cell to 0
+	callButtons[index] = 0;
 }
 
 
@@ -195,6 +225,133 @@ int set_destination(){
     * (3) If there are also no pending elevator buttons then return 0 to put the
     *     lift into an IDLE state.
     */
+
+
+	int i, j, k, l, m, n;
+	int noCallButtons;
+
+	//------Down info---------//
+	//If there is a service call down, find the nearest floor
+	int serviceDown = 0;
+	int serviceDownFlag = 0;
+	for (i = 1; i < (currentFloor*2) - 1; i = i + 2) {
+		if (callButtons[i] != 0) {
+			serviceDown = i;
+			serviceDownFlag = 1;
+		}
+	}
+	//If there is a elevator button request down from current floor
+	int elevatorDown = 0;
+	int elevatorDownFlag = 0;
+	for (j = 0; j <= currentFloor - 2; j++) {
+		if (elevatorButtons[j] != 0) {
+			elevatorDown = j + 1; //store the closest down request inside the elevator
+			elevatorDownFlag = 1;
+		}
+	}
+
+
+	//------Up info---------//
+	//If there is a service call up, find the nearest floor
+	int serviceUp = 0;
+	int serviceUpFlag = 0;
+	for (k = 6; k > (currentFloor*2) - 2; k = k - 2) {
+		if (callButtons[k] != 0) {
+			serviceUp = k+1;
+			serviceUpFlag = 1;
+		}
+	}
+	//If there is a elevator button request up from current floor
+	int elevatorUp = 0;
+	int elevatorUpFlag = 0;
+	for (l = 3; l > currentFloor - 1; l--) {
+		if (elevatorButtons[l] != 0) {
+			elevatorUp = l+1; //store the closest up request inside the elevator
+			elevatorUpFlag = 1;
+		}
+	}
+
+
+	//------Move down---------//
+	if ((motorDirection == DOWN) && ((serviceDownFlag != 0) || (elevatorDownFlag != 0))) {
+		//If there is a call down request on floor below
+		if (callButtons[(currentFloor*2) - 3] != 0) {
+			requestedFloor = currentFloor - 1;
+			requestedDirection = DOWN;;
+			return 1;
+		}
+		//Find the closest floor down
+		if (((serviceDown+1)/2) > elevatorDown && (elevatorDown != 0)){
+			requestedFloor = (serviceDown+1)/2;
+		} else {
+			requestedFloor = elevatorDown;
+		}
+		requestedDirection = DOWN;
+		return 1;
+	}
+	//-----Move up---------//
+	else if ((motorDirection == UP) && ((serviceUpFlag != 0) || (elevatorUpFlag != 0))){
+		//If there is a call up request on floor above
+		if (callButtons[currentFloor * 2] != 0) {
+			requestedFloor = currentFloor + 1;
+			requestedDirection = UP;
+			return 1;
+		}
+		//Find the closest floor up
+		if (((serviceUp+2)/2) > elevatorUp && (elevatorUp != 0)){
+			requestedFloor = elevatorUp;
+		} else {
+			requestedFloor = (serviceUp+2)/2;
+		}
+		requestedDirection = UP;
+		return 1;
+	}
+	//-----Other requests---------//
+	else {
+		//Finding the index of the highest priority call
+		int priorityIndex = 0;
+		int priorityFlag = 0;
+		for (m = 0; m < 8; m++) {
+			if (callButtons[m] == 1) {
+				priorityIndex = m;
+				priorityFlag = 1;
+				break;
+			}
+		}
+
+		//Checking if there are any floor service requests
+		for (n = 0; n <= 3; n++) {
+			if (elevatorButtons[n] != 0) {
+				noCallButtons = 0;
+				break;
+			} else {
+				noCallButtons = 1;
+			}
+		}
+
+		if (priorityFlag != 0) {
+			if ((priorityIndex % 2) == 1) { //Then highest priority call is a down call
+				requestedFloor = ((priorityIndex - 1) / 2) + 1;
+				requestedDirection = DOWN;
+				return 1;
+			} else {
+				requestedFloor = (priorityIndex / 2) + 1;
+				requestedDirection = UP;
+				return 1;
+			}
+		} else if (noCallButtons == 0) {
+			requestedFloor = n + 1;
+			if (currentFloor < requestedFloor) {
+				requestedDirection = UP;
+			} else {
+				requestedDirection = DOWN;
+			}
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	return 0;
 }
 
 
@@ -264,7 +421,7 @@ int store_new_requests(){
 
     //wait for floor of request to follow
 
-    while((uartCharacter = getc(fp)) == '\0'){}
+     while((uartCharacter = getc(fp)) == '\0'){}
 
     switch (uartCharacter){
       case '1':
@@ -297,9 +454,11 @@ int store_new_requests(){
     //store request in array
     if (buttonDirection == IDLE){
       elevatorButtons[buttonFloor-1] = 1;
+      printf("button on %d\n", buttonFloor);
     }
     else{
-      callButtons_add(buttonFloor, buttonDirection);
+    	printf("call on %d\n", buttonFloor);
+		callButtons_add(buttonFloor, buttonDirection);
     }
     return(1);
   }
@@ -343,62 +502,76 @@ void print_elevator_status(){
  * Outputs:     None
  ************************************************************************/
 int main(void){
+
+
+	FILE *lcd;
+	lcd = fopen(LCD_NAME, "w");
+	fprintf(lcd, "Welcome to the lift controller\n\n");
+	printf("Lift controller is live\n");
+	fclose(lcd);
+
   int newRequest = 0, destinationValid = 0;
   char* formattedString = "NIOS II ELEVATOR CONTROLLER\n";
 
 
     fp = fopen("/dev/uart", "r+");
-    fprintf(fp,"%s",formattedString );
+
+	if (fp != NULL) {
+		fprintf(fp,"%s",formattedString );
+	}
 
   motor_control();
   init_button_interrupts();
 
-
-
-
-
   while(1){
+
 	/*
 	  * store_new_requests uses getc to read UART input.
-	  * This is a blocking read. Hence, this will stall the 
+	  * This is a blocking read. Hence, this will stall the
 	  * program. To prevent this, implement a condition
-	  * over the function call ("newRequest = store_new_requests();". 
-	  * The condition being when switch 17 (SW17) 
-	  * is switch on to be ture.
+	  * over the function call ("newRequest = store_new_requests();".
+	  * The condition being when switch 17 (SW17)
+	  * is switch on to be true.
 	*/
-	
-	//if (SW17==ON) { implement the condition
-	//}
-	if (IORD_ALTERA_AVALON_PIO_DATA(SWITCHES_BASE) == 65536){
+
+	if (IORD_ALTERA_AVALON_PIO_DATA(SWITCHES_BASE) == 131072) {
+		printf("Switch 17 is live\n");
 		newRequest = store_new_requests();
 	}
-	
+
     if(newRequest){
-      destinationValid = set_destination();
-      motor_control();
-      print_elevator_status();
+		destinationValid = set_destination();
+		motor_control();
+		print_elevator_status();
+		newRequest = 0;
     }
 
-    if(requestedFloor == currentFloor && destinationValid){
-      callButtons_remove(requestedFloor, requestedDirection);
-      elevatorButtons[requestedFloor-1] = 0;
-      formattedString = "Opening Door\n";
+    //printf("Current floor is: %d. Requested floor is: %d \n", currentFloor, requestedFloor);
 
-    fprintf(fp,"%s",formattedString);
+    if(requestedFloor == currentFloor && destinationValid && !doorOpen){
+		callButtons_remove(requestedFloor, requestedDirection);
+		elevatorButtons[requestedFloor-1] = 0;
+		formattedString = "Opening Door\n";
 
-      usleep(5000000);
-      formattedString = "Closing Door\n";
+		fprintf(fp,"%s",formattedString);
 
+		//usleep(1000000);
+		//formattedString = "Closing Door\n";
+		//fprintf(fp,"%s",formattedString);
 
-    fprintf(fp,"%s",formattedString);
+		doorOpen = 1;
 
+		alt_alarm_start(&timer, 5000, timeout_function, 0);
 
-      //store_new_requests();
-      destinationValid = set_destination();
-      motor_control();
-      print_elevator_status();
+		//store_new_requests();
+		destinationValid = set_destination();
+		motor_control();
+		print_elevator_status();
     }
+
   }
+
+  fclose(fp);
 
   return 0;
 }
